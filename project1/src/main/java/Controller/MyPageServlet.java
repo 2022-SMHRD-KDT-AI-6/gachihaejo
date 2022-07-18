a02ee44945c4daf8234e3808178546d9a3dbc89a
smhrd <smhrd@DESKTOP-8K7J55J>
2022-07-18 15:26:32 +0900

    MemberDTO 수정

--- project1/src/main/java/Controller/MyPageServlet.java
+++ project1/src/main/java/Controller/MyPageServlet.java
@@ -12,8 +12,9 @@ import javax.websocket.Session;
 import com.oreilly.servlet.MultipartRequest;
 import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
 
+import Model.LoginDTO;
 import Model.MemberDAO;
-import Model.MemberDTO;
+
 
 
 @WebServlet("/MyPageServlet")
@@ -44,36 +45,36 @@ public class MyPageServlet extends HttpServlet {
 				new DefaultFileRenamePolicy() // 중복된 파일 이름 발생 시 자동으로 이름 변환
 				);
 		
-		String user_pw = multi.getParameter("user_pw");
-		String user_nick = multi.getParameter("user_nick");
-		String user_phone = multi.getParameter("user_phone");
-		String user_email = multi.getParameter("user_email");
-		String user_profile = multi.getParameter("user_profile");
+		String pw = multi.getParameter("pw");
+		String nick = multi.getParameter("nick");
+		String phone = multi.getParameter("phone");
+		String email = multi.getParameter("email");
+		String profile = multi.getParameter("profile");
 		
-		String user_photo = multi.getFilesystemName("file");
-		System.out.println("전송 받은 파일 : " + user_photo);
+		String photo = multi.getFilesystemName("file");
+		System.out.println("전송 받은 파일 : " + photo);
 		
 		// 내가 확인하면서 할 것
 //		MemberDAO dao = new MemberDAO();
 //		MemberDTO user = dao.select("user_id");
 		
-		System.out.println("비밀번호 : " + user_pw);
-		System.out.println("닉네임 : " + user_nick);
-		System.out.println("번호 : " + user_phone);
-		System.out.println("이메일 : " + user_email);
-		System.out.println("프로필 : " + user_profile);
-		System.out.println("사진 : " + user_photo);
+		System.out.println("비밀번호 : " + pw);
+		System.out.println("닉네임 : " + nick);
+		System.out.println("번호 : " + phone);
+		System.out.println("이메일 : " + email);
+		System.out.println("프로필 : " + profile);
+		System.out.println("사진 : " + photo);
 		
 		
 		// 은영이꺼랑 합치면 쓰장
 		HttpSession session = request.getSession();
-		MemberDTO dto = (MemberDTO)session.getAttribute("user_id");
+		LoginDTO dto = (LoginDTO)session.getAttribute("id");
 //		String user_id = user.getUser_id(); -> 확인
-		String user_id = dto.getUser_id();
+		String id = dto.getId();
 		
-		System.out.println("아이디 : " + user_id);
+		System.out.println("아이디 : " + id);
 		
-		MemberDTO udto = new MemberDTO(user_id, user_pw, user_nick, user_phone, user_email, user_profile, user_photo);
+		LoginDTO udto = new LoginDTO(id, pw, nick, phone, email, profile, photo);
 		
 		MemberDAO dao = new MemberDAO();
 		// -> 합치고 나서 사용!
@@ -83,12 +84,12 @@ public class MyPageServlet extends HttpServlet {
 			// 성공 : cnt = 1
 			System.out.println("개인정보 수정 성공");
 			
-			System.out.println("수정 비번 : " + udto.getUser_pw());
-			System.out.println("수정 닉 : " + udto.getUser_nick());
-			System.out.println("수정 번호 : " + udto.getUser_phone());
-			System.out.println("수정 메일 : " + udto.getUser_email());
-			System.out.println("수정 프로필 : " + udto.getUser_profile());
-			System.out.println("수정 사진 : " + udto.getUser_photo());
+			System.out.println("수정 비번 : " + udto.getPw());
+			System.out.println("수정 닉 : " + udto.getNick());
+			System.out.println("수정 번호 : " + udto.getPhone());
+			System.out.println("수정 메일 : " + udto.getEmail());
+			System.out.println("수정 프로필 : " + udto.getProfile());
+			System.out.println("수정 사진 : " + udto.getPhoto());
 			
 			// 세션에 있는 dto 덮어쓰기
 			// 저장하는 이름이 겹쳤을 경우 -> 덮어쓰기
